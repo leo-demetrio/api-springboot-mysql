e7b58a5fa61190d5039df16781d9da6d6c9f5ae9
leo-demetrio <leocdemetrio@gmail.com>
2021-11-22 09:02:38 -0300

    feat: add validation universal

--- src/main/java/project/base/studiesspring/requests/ProductPostRequestBody.java
+++ src/main/java/project/base/studiesspring/requests/ProductPostRequestBody.java
@@ -2,7 +2,12 @@ package project.base.studiesspring.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class ProductPostRequestBody {
+    @NotNull(message = "The product name cannot be null")
+    @NotEmpty(message = "The product name cannot be null")
     private String name;
 }
