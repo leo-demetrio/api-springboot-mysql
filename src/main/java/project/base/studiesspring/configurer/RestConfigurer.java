8bee4686001b5c1d7230b3c2613c48eb36b56020
leo-demetrio <leocdemetrio@gmail.com>
2021-11-25 18:18:09 -0300

    build: create RestConfigurer e add pagination and debug sql

--- src/main/java/project/base/studiesspring/configurer/RestConfigurer.java
+++ src/main/java/project/base/studiesspring/configurer/RestConfigurer.java
@@ -2,7 +2,6 @@ package project.base.studiesspring.configurer;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.web.PageableArgumentResolver;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
